7095f0132b5fc813b55bd6f34e52d9e6375f38df
HyperMachinex <armagan.trs@protonmail.ch>
2025-08-13 17:40:05 +0300

    readability improve

--- time_server/time_server.c
+++ time_server/time_server.c
@@ -18,9 +18,10 @@
 #define SOCKET int
 #define GETSOCKETERRNO() (errno)
 
+#define LINE "==============================\n"
 int main(int argc, char *argv[]){
   
-  printf("Configuring local address...\n");
+  printf("Configuring local address...\n%s", LINE);
   struct addrinfo hints;
   memset(&hints, 0, sizeof(hints));
   hints.ai_family = AF_INET;
@@ -30,7 +31,7 @@ int main(int argc, char *argv[]){
   struct addrinfo *bind_address;
   getaddrinfo(0, "8080", &hints, &bind_address);
 
-  printf("Creating socket...\n");
+  printf("Creating socket...\n%s", LINE);
   SOCKET socket_listen;
   socket_listen = socket(bind_address->ai_family, bind_address->ai_socktype, bind_address->ai_protocol);
   if(!ISVALIDSOCKET(socket_listen)){
@@ -38,7 +39,7 @@ int main(int argc, char *argv[]){
     return 1;
   }
   
-  printf("Binding socket to local address...\n");
+  printf("Binding socket to local address...\n%s", LINE);
   if(bind(socket_listen, bind_address->ai_addr, bind_address->ai_addrlen)){
     fprintf(stderr, "bind() failed (%d)\n", GETSOCKETERRNO());
     return 1;
@@ -46,13 +47,13 @@ int main(int argc, char *argv[]){
 
   freeaddrinfo(bind_address);
 
-  printf("Listening...\n");
+  printf("Listening...\n%s", LINE);
   if(listen(socket_listen, 10) < 0) {
     fprintf(stderr, "listen() failed. (%d)\n", GETSOCKETERRNO());
     return 1;
   }
 
-  printf("Waiting for connection...\n");
+  printf("Waiting for connection...\n%s", LINE);
   struct sockaddr_storage client_address;
   socklen_t client_len = sizeof(client_address);
   SOCKET socket_client = accept(socket_listen, (struct sockaddr*) &client_address, &client_len);
@@ -61,18 +62,18 @@ int main(int argc, char *argv[]){
     return 1;
   }
 
-  printf("Client is connected...");
+  printf("Client is connected:");
   char address_buffer[100];
   getnameinfo((struct sockaddr*)&client_address, client_len, address_buffer, sizeof(address_buffer), 0, 0, NI_NUMERICHOST);
-  printf("%s\n", address_buffer);
+  printf("%s\n%s", address_buffer, LINE);
 
-  printf("Reading request..\n");
+  printf("Reading request..\n%s", LINE);
   char request[1024];
   int bytes_received = recv(socket_client, request, 1024, 0);
-  printf("Received %d bytes.\n", bytes_received);
-  printf("%.*s", bytes_received, request);
+  printf("Received %d bytes.\n%s", bytes_received, LINE);
+  //printf("%.*s", bytes_received, request);
 
-  printf("Sending response...\n");
+  printf("Sending response...\n%s", LINE);
   const char* response = 
                         "HTTP/1.1 200 OK\r\n"
                         "Connection: close\r\n"
@@ -80,18 +81,18 @@ int main(int argc, char *argv[]){
                         "Local time is: ";
 
   int bytes_sent = send(socket_client, response, strlen(response), 0);
-  printf("Sent %d of %d bytes.\n", bytes_sent, (int)strlen(response));
+  printf("Sent 200OK %d of %d bytes.\n", bytes_sent, (int)strlen(response));
 
   time_t timer;
   time(&timer);
   char *time_msg = ctime(&timer);
   bytes_sent = send(socket_client, time_msg, strlen(time_msg), 0);
-  printf("Sent %d of %d bytes.\n", bytes_sent, (int)strlen(time_msg));
+  printf("Sent time_msg %d of %d bytes.\n%s", bytes_sent, (int)strlen(time_msg), LINE);
 
-  printf("Closing connection...\n");
+  printf("Closing connection...\n%s", LINE);
   CLOSESOCKET(socket_client);
 
-  printf("Closing listening socket...\n");
+  printf("Closing listening socket...\n%s", LINE);
   CLOSESOCKET(socket_listen);
 
   printf("F I N I S H E D\n");
